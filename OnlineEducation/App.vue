30235b02aee494fabbbdd0b2080c85aa1060adcd
hu <1182830389@qq.com>
2022-12-01 16:01:30 +0800

    创建项目，书写tabber

---
+++ OnlineEducation/App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*公共页面样式 */
+	@import url("@/common/free.css");
+	/* 图标 */
+	@import url("~@/common/iconfont.css");
+	/* 主颜色 */
+	@import url("@/common/common.css");
+	/* 动画 */
+	@import url("@/common/animate.min.css");
+</style>
---
+++ OnlineEducation/pages/learner/learner.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		学习
+		<text class="iconfont icon-fanhui icon-bofang"></text>
+		<view class="">
+			<text class="iconfont view text-main icon-add">123456</text>
+		</view>
+		<view class="animate__animated animate__bounceInLeft">
+			123456789
+		</view>
+		<view class="animate__animated animate__backInUp">
+			000000
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
